705276809957b3b090eaac4384f87538c7a741a2
wukddang <43228743+funkyblues@users.noreply.github.com>
2022-07-31 02:06:25 +0900

    signup implementation with createview

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"
 # 쓰는 이유? -> 개발 중에는 "127.0.0.1:8000/account/hello_world"
@@ -7,5 +7,6 @@ app_name = "accountapp"
 # django 내에서 라우팅을 편하게 해주는 함수가 있어서 저걸 쓴다고 하는군..
 
 urlpatterns = [
-  path('hello_world/', hello_world, name='hello_world')
+  path('hello_world/', hello_world, name='hello_world'),
+  path('create/', AccountCreateView.as_view(), name='create'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,13 @@
+# django에서 기본적으로 제공하는 User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+# django에서 제공하는 CreateView 가져오기
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -12,7 +17,7 @@ def hello_world(request):
 
     temp = request.POST.get('hello_world_input')
 
-    # HelloWorld 클래스로 새로운 객체 생성
+    # models.py의 HelloWorld 클래스로 새로운 객체 생성
     new_hello_world = HelloWorld()
     # POST로 전달한 데이터를 생성한 객체의 text에 대입
     new_hello_world.text = temp
@@ -24,3 +29,14 @@ def hello_world(request):
     # HelloWorld의 모든 데이터를 다 가져온다
     hello_world_list = HelloWorld.objects.all()
     return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+class AccountCreateView(CreateView):
+  # django에서 기본 제공하는 모델 사용
+  model = User
+  # django에서 기본적으로 제공하는 계정 생성 폼
+  form_class = UserCreationForm
+  # 계정 생성에 성공했다면 리다이렉트 할 경로 지정
+  # 왜 reverse_lazy? => 함수와 클래스가 불러와지는 방식의 차이 때문...
+  # 클래스 형 view에서 reverse_lazy를 사용한다.
+  success_url = reverse_lazy('accountapp:hello_world')
+  template_name = 'accountapp/create.html'
