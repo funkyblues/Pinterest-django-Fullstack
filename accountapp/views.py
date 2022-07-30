26bb89381dba3650aa68aa29a0fbf966f29e4463
wukddang <43228743+funkyblues@users.noreply.github.com>
2022-07-31 01:12:27 +0900

    get data from DB / django template for loop

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -10,7 +12,6 @@ def hello_world(request):
 
     temp = request.POST.get('hello_world_input')
 
-
     # HelloWorld 클래스로 새로운 객체 생성
     new_hello_world = HelloWorld()
     # POST로 전달한 데이터를 생성한 객체의 text에 대입
@@ -18,7 +19,8 @@ def hello_world(request):
     # save를 하여 실제 DB에 new_hello_world 객체(!!!)를 저장(!!!)하게 된다.
     new_hello_world.save()
 
-
-    return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+    return HttpResponseRedirect(reverse('accountapp:hello_world'))
   else:
-    return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+    # HelloWorld의 모든 데이터를 다 가져온다
+    hello_world_list = HelloWorld.objects.all()
+    return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
