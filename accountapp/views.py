f7b776bc9a50f23190101fc1df9fdd3631aff79d
wukddang <43228743+funkyblues@users.noreply.github.com>
2022-07-31 23:12:43 +0900

    context_object_user 내용 추가

--- accountapp/views.py
+++ accountapp/views.py
@@ -51,17 +51,21 @@ class AccountDetailView(DetailView):
   # 다른사람이 와도 나의 정보를 보여줄 수 있도록...(?)
 
   # 템플릿 파일에서 사용할 컨텍스트 변수
+  # 즉 현재 방문하고 있는 유저의 정보 = context_object_name
   context_object_name = 'target_user'
   template_name = 'accountapp/detail.html'
 
+# Update, Delete에도 context_object_name을 넣어준다
 
 class AccountUpdateView(UpdateView):
   model = User
+  context_object_name = 'target_user'
   form_class = AccountUpdateForm
   success_url = reverse_lazy('accountapp:hello_world')
   template_name = 'accountapp/update.html'
 
 class AccountDeleteView(DeleteView):
   model = User
+  context_object_name = 'target_user'
   success_url = reverse_lazy('accountapp:login')
   template_name = 'accountapp/delete.html'
